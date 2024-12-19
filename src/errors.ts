ef3f1aac35850fef61053a684c828a3c0422b24f
nuintun <nuintun@qq.com>
2024-12-19 10:37:22 +0800

    feat: Update code

--- src/errors.ts
+++ src/errors.ts
@@ -4,20 +4,20 @@
 
 // 未支持的编码格式
 export function encodingInvalid(encoding: string): string {
-  return 'Unsupported encoding ' + encoding;
+  return 'unsupported encoding ' + encoding;
 }
 
 // 未知字节序
-export const unknownEndianness = 'Unknown endianness';
+export const unknownEndianness = 'unknown endianness';
 
 // 非法长度
-export const lengthInvalid = 'Invalid buffer length';
+export const lengthInvalid = 'invalid buffer length';
 
 // 非法读写指针
-export const offsetInvalid = 'Invalid buffer offset';
+export const offsetInvalid = 'invalid buffer offset';
 
 // 数据读取溢出
-export const readOverflow = 'Read is outside the bounds of the Buffer';
+export const readOverflow = 'read is outside the bounds of the Buffer';
 
 // 读写指针溢出
-export const offsetOverflow = 'Offset is outside the bounds of the Buffer';
+export const offsetOverflow = 'offset is outside the bounds of the Buffer';
